6ed07dae8250383e5bff20ea5c8e3c8f7cab6aca
Pierre <pierre.massonie@etu.unilim.fr>
2023-06-19 10:18:14 +0200

    [main] Added main structure

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+
+   // Struct Word = getRandomWord()
+   char guess = '0';
+   char* mysteryWord;
+
+   int vie = 9;
+
+   do{
+      printf("Please guess a character: ");
+      // in char
+      
+      //if(checkChara(guess)) {}
+         print("%c is correct!\n%s");
+      //else {}
+         print("%c is incorect!\n\n%s");
+         vie--;
+      
+      // if Check word is complete
+         // print Victory
+      // if(vie == 0)
+      // print FAILURE
+      // Break
+
+   }while(true);
+   
    return 0;
 }
