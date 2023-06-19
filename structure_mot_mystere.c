13f5b0fdaa8fbab11baad2040146021f6d9d39e5
thomas <thomas.nicolazo@gmail.com>
2023-06-19 12:48:18 +0200

    fix typos in code

--- structure_mot_mystere.c
+++ structure_mot_mystere.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include "structure_mot_mystere.h"
 
-struct MysteryWord * generateMysteryWord(){
+struct MysteryWord  generateMysteryWord(){
         /*
          */
         FILE *fptr;
@@ -14,14 +14,14 @@ struct MysteryWord * generateMysteryWord(){
                 printf("file doesn't exist");
                 exit(0);
         }
-	struct MysteryWord  *motATrouver = {"",0};
+	struct MysteryWord  motATrouver = {"",0};
 	srand(time(NULL)); // Initialization random generator
 	int lineMysteryWord = rand() % maxLineFile; // number of line in file containing words 
 	int charWordCounter = 0;// count char in line
 	int lineCounter = 0; // count nb line when fuènction reads char
 	while(ch != EOF){
-		if(ch == '\n'i && lineCounter != lineMysteryWord){
-			lineCouter++;
+		if(ch == '\n' && lineCounter != lineMysteryWord){
+			lineCounter++;
 			charWordCounter = 0;
 		}
 		else  if(lineCounter == lineMysteryWord){
@@ -29,7 +29,7 @@ struct MysteryWord * generateMysteryWord(){
 		}
 		ch = fgetc(fptr);
 		motATrouver.word[charWordCounter] = ch;
-		motATrouver.lenghtWords = charWordCounter;
+		motATrouver.lengthWords = charWordCounter;
 		charWordCounter++;
 			
 	}	
