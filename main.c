d1942c91fa9fd18263644ca513892c70c499c0e3
Pierre <pierre.massonie@etu.unilim.fr>
2023-06-19 10:36:16 +0200

    [main] added input, better structure

--- main.c
+++ main.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
+#include "structure_mot_mystere.h"
 
 int main() {
 
    // Struct Word = getRandomWord()
    char guess = '0';
-   char* mysteryWord;
+   char* mysteryWord; //Malloc word size
 
-   int vie = 9;
+   int life = 9; // Hardcoded for now
 
    do{
+      // We take the user character guess
       printf("Please guess a character: ");
-      // in char
+      scanf("%s", &guess);
       
-      //if(checkChara(guess)) {}
-         print("%c is correct!\n%s");
-      //else {}
-         print("%c is incorect!\n\n%s");
-         vie--;
-      
-      // if Check word is complete
-         // print Victory
-      // if(vie == 0)
-      // print FAILURE
-      // Break
+      // We check if this guess is contained in the word
+      if(true) { //checkChara(guess)
+         print("%c is correct!\n", guess);
+         print("%s\n",  mysteryWord);
+      }
+      else {
+         print("%c is incorect!\n", guess);
+         print("Only %i lifes remainings.\n", --life);
+         print("%s\n", mysteryWord);
+      }
+
+      // If the word is complettely discovered, user win
+      if(true){ //strcmp(mysteryWord, Word.mot) is complete
+         print("You have guessed the word %s.\nVICTORY!", mysteryWord);
+         break;
+      }
+
+      // If the user is out of lifes, user lose
+      if(life <= 0){
+         print("You are out of lifes...\nFAILURE!");
+         print("    ============\n    || //    |\n\n    ||//     |\n    ||/      |\n    ||       O\n    ||      /|\\\n||       |\n    ||      / \\\n    ||\n    ||\n    ||\n==========");
+         break;
+      }
 
    }while(true);
    
+   print("Thank you for playing");
+
    return 0;
 }
