d1c891ce2e9e79c67a9e4415e673a5bf39adee4a
lxnr-p <al.poirier@outlook.com>
2023-06-19 11:51:10 +0200

    implementation fonction checkChara

--- checkMotMystere.c
+++ checkMotMystere.c
@@ -1,8 +1,22 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "structure_mot_mystere.h"
 
 
-int	main()
+
+bool checkChara(char c, struct MysteryWord* word, char* censoredWord)
 {
-	char charInput; //lettre saisie
+	bool isFound = false;
+
+	for (int i = 0; i < word->lengthWords; i++)
+	{
+		if (word->word[i] == c)
+		{
+			isFound = true;
+			censoredWord[i] = word->word[i];
+		}
+	}
+
+	return isFound;
 }
---
+++ checkMotMystere.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "structure_mot_mystere.h"
+
+bool checkChara(char c, struct MysteryWord* word, char* censoredWord);
